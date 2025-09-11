f80ccc6c89f00235d8760faba0bc2588e1ed1fec
Antonin <antonin63170@gmail.com>
2025-09-11 15:28:44 +0200

    Add profile edit page and edit button

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -8,49 +8,64 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+#[Route('/profile')]
 final class ProfileController extends AbstractController
 {
     use UserRedirectionTrait;
 
-    #[Route('/profile', name: 'app_profile_redirect')]
+    #[Route('', name: 'app_profile_redirect')]
     public function redirectToUserProfile(): Response
     {
+        /** @var User|null $user */
         $user = $this->getUser();
 
         if (!$user) {
             return $this->redirectToRoute('app_login');
         }
 
-        if (!$user instanceof User || !$user->getProfile() || !$user->getProfile()->getUsername()) {
+        if (!$user->getProfile()?->getUsername()) {
             return $this->redirectToRoute('app_complete');
         }
 
         return $this->redirectToRoute('app_profile', ['username' => $user->getProfile()->getUsername()]);
     }
 
-    #[Route('/profile/{username}', name: 'app_profile')]
+    #[Route('/{username}', name: 'app_profile')]
     public function profile(string $username): Response
     {
-        $redirectResponse = $this->checkUserAccess();
-        if ($redirectResponse !== null) {
-            return $redirectResponse;
-        }
-
-        return $this->render('profile/profile.html.twig', [
-            'username' => $username,
-        ]);
+        return $this->handleProfileAction($username, 'profile/profile.html.twig');
     }
 
-    #[Route('/profile/{username}/posts', name: 'app_profile_posts')]
+    #[Route('/{username}/posts', name: 'app_profile_posts')]
     public function profilePosts(string $username): Response
     {
+        return $this->handleProfileAction($username, 'profile/profile-posts.html.twig');
+    }
+
+    #[Route('/{username}/edit', name: 'app_profile_edit')]
+    public function profileEdit(string $username): Response
+    {
         $redirectResponse = $this->checkUserAccess();
-        if ($redirectResponse !== null) {
+        if ($redirectResponse) {
             return $redirectResponse;
         }
 
-        return $this->render('profile/profile-posts.html.twig', [
-            'username' => $username,
-        ]);
+        /** @var User $user */
+        $user = $this->getUser();
+        if ($user->getProfile()->getUsername() !== $username) {
+            return $this->redirectToRoute('app_profile', ['username' => $user->getProfile()->getUsername()]);
+        }
+
+        return $this->render('profile/profile-edit.html.twig', ['username' => $username]);
+    }
+
+    private function handleProfileAction(string $username, string $template, bool $requireOwnership = false): Response
+    {
+        $redirectResponse = $this->checkUserAccess();
+        if ($redirectResponse) {
+            return $redirectResponse;
+        }
+
+        return $this->render($template, ['username' => $username]);
     }
 }
