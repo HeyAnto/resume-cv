dc5b50a2ae39d7a3faa033b1ffb9ea762e9dc252
Antonin <antonin63170@gmail.com>
2025-09-15 14:31:14 +0200

    Add character counters and validation to experience form

--- src/Form/ExperienceFormType.php
+++ src/Form/ExperienceFormType.php
@@ -11,6 +11,8 @@ use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\UrlType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Validator\Context\ExecutionContextInterface;
 
 class ExperienceFormType extends AbstractType
 {
@@ -21,7 +23,16 @@ class ExperienceFormType extends AbstractType
         'label' => 'Company Name',
         'attr' => [
           'placeholder' => 'Google, Microsoft, etc.',
-          'class' => 'form-control'
+          'class' => 'form-control',
+          'data-counter' => 'companyName-counter',
+          'data-max' => '32'
+        ],
+        'constraints' => [
+          new Assert\NotBlank(['message' => 'Company name is required']),
+          new Assert\Length([
+            'max' => 32,
+            'maxMessage' => 'Company name cannot be longer than {{ limit }} characters'
+          ])
         ]
       ])
       ->add('companyLink', UrlType::class, [
@@ -29,21 +40,47 @@ class ExperienceFormType extends AbstractType
         'required' => false,
         'attr' => [
           'placeholder' => 'https://www.example.com',
-          'class' => 'form-control'
+          'class' => 'form-control',
+          'data-counter' => 'companyLink-counter',
+          'data-max' => '98'
+        ],
+        'constraints' => [
+          new Assert\Length([
+            'max' => 98,
+            'maxMessage' => 'Company website cannot be longer than {{ limit }} characters'
+          ])
         ]
       ])
       ->add('job', TextType::class, [
         'label' => 'Job Title',
         'attr' => [
           'placeholder' => 'Full Stack Developer',
-          'class' => 'form-control'
+          'class' => 'form-control',
+          'data-counter' => 'job-counter',
+          'data-max' => '32'
+        ],
+        'constraints' => [
+          new Assert\NotBlank(['message' => 'Job title is required']),
+          new Assert\Length([
+            'max' => 32,
+            'maxMessage' => 'Job title cannot be longer than {{ limit }} characters'
+          ])
         ]
       ])
       ->add('location', TextType::class, [
         'label' => 'Location',
         'attr' => [
           'placeholder' => 'Paris, France',
-          'class' => 'form-control'
+          'class' => 'form-control',
+          'data-counter' => 'location-counter',
+          'data-max' => '32'
+        ],
+        'constraints' => [
+          new Assert\NotBlank(['message' => 'Location is required']),
+          new Assert\Length([
+            'max' => 32,
+            'maxMessage' => 'Location cannot be longer than {{ limit }} characters'
+          ])
         ]
       ])
       ->add('description', TextareaType::class, [
@@ -52,7 +89,15 @@ class ExperienceFormType extends AbstractType
         'attr' => [
           'placeholder' => 'Describe your missions and achievements...',
           'class' => 'form-control',
-          'rows' => 3
+          'rows' => 3,
+          'data-counter' => 'description-counter',
+          'data-max' => '120'
+        ],
+        'constraints' => [
+          new Assert\Length([
+            'max' => 120,
+            'maxMessage' => 'Description cannot be longer than {{ limit }} characters'
+          ])
         ]
       ])
       ->add('startDate', DateType::class, [
@@ -79,6 +124,28 @@ class ExperienceFormType extends AbstractType
   {
     $resolver->setDefaults([
       'data_class' => Experience::class,
+      'constraints' => [
+        new Assert\Callback([$this, 'validateDates'])
+      ]
     ]);
+  }
+
+  public function validateDates($experience, ExecutionContextInterface $context): void
+  {
+    if ($experience->getStartDate() && $experience->getEndDate()) {
+      if ($experience->getStartDate() > $experience->getEndDate()) {
+        $context->buildViolation('End date must be after start date')
+          ->atPath('endDate')
+          ->addViolation();
+      }
+    }
+
+    // Check if dates are not in the future
+    $now = new \DateTimeImmutable();
+    if ($experience->getStartDate() && $experience->getStartDate() > $now) {
+      $context->buildViolation('Start date cannot be in the future')
+        ->atPath('startDate')
+        ->addViolation();
+    }
   }
 }
